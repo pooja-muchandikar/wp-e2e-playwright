a52dd187a6569e7ff647be241010a8a6abcc9e3d
juhi saxena <juhi.saxena@rtcamp.com>
2022-07-22 13:03:55 +0530

    Fix formatting of add-new-post

--- specs/add-new-post.spec.js
+++ specs/add-new-post.spec.js
@@ -1,26 +1,26 @@
 /**
  * WordPress dependencies
  */
-const { test, expect } = require("@wordpress/e2e-test-utils-playwright");
+ const { test, expect } = require( '@wordpress/e2e-test-utils-playwright' );
 
-test.describe('Add New Post', () => {
-  test.beforeEach(async ({ page, admin }) => {
+test.describe ( 'Add New Post', ( ) => {
+  test.beforeEach( async ( { page, admin } ) => {
     // await loginUser();
     await admin.createNewPost();
     await page.$('[placeholder="Add title"]');
   });
 
-  test('Should create new post', async ({ page }) => {
-    await page.type('.editor-post-title__input', 'Test Post');
+  test('Should create new post', async ( { page } ) => {
+    await page.type( '.editor-post-title__input', 'Test Post' );
     // await page.type('p.block-editor-rich-text__editable editor-rich-text__editable.wp-block-paragraph','This is the test post');
 
     //Click on publish button
-    await page.click('.editor-post-publish-panel__toggle');
+    await page.click( '.editor-post-publish-panel__toggle' );
 
     //Double check, click again on publish button
-    await page.click('.editor-post-publish-button');
+    await page.click( '.editor-post-publish-button' );
 
     // A success notice should show up
-    page.locator('.components-snackbar');
+    page.locator( '.components-snackbar' );
   });
 });
