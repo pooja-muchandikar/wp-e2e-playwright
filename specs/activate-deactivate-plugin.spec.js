93b46a0866e6760a01ce205c0678b38f35657948
pooja-muchandikar <pooja.muchandikar@rtcamp.com>
2022-07-22 12:47:12 +0530

    Update quotes

--- specs/activate-deactivate-plugin.spec.js
+++ specs/activate-deactivate-plugin.spec.js
@@ -5,12 +5,12 @@ const { test, expect } = require("@wordpress/e2e-test-utils-playwright");
 
 const timeout = 10000;
 const slug = "hello-dolly";
-test.describe("Activate Deactivate Plugin", () => {
-  test(" should activate plugin successfully", async ({ requestUtils }) => {
+test.describe('Activate Deactivate Plugin', () => {
+  test('should activate plugin successfully', async ({ requestUtils }) => {
     await requestUtils.activatePlugin(slug);
   });
 
-  test(" should deactivate plugin successfully", async ({ requestUtils }) => {
+  test('should deactivate plugin successfully', async ({ requestUtils }) => {
     await requestUtils.deactivatePlugin(slug);
   });
 });
