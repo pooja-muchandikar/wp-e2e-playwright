7092b3c69a7ecf42cbac82cb4b5fc40fbca08081
pavanpatil1 <=>
2022-07-29 10:50:57 +0530

    Addressed review feedback

--- specs/upload-media.spec.js
+++ specs/upload-media.spec.js
@@ -7,7 +7,7 @@ test.describe('Upload Media', () => {
   test('Should able to upload media', async ({ page, admin }) => {
     await admin.visitAdminPage('media-new.php')
 
-    page.locator("div[class='wrap'] h1") //Wait for Upload media text
+    await expect(page.locator( "div[class='wrap'] h1" )).toHaveText( 'Upload Media' ) //Wait for Upload media text
 
     const [fileChooser] = await Promise.all([
       // It is important to call waitForEvent before click to set up waiting.
@@ -15,11 +15,9 @@ test.describe('Upload Media', () => {
       // Opens the file chooser.
       page.locator('role=button[name="Select Files"i]').click()
     ])
-    await fileChooser.setFiles(['img.jpg'])
+    await fileChooser.setFiles(['uploads/img.jpg'])
 
     //Validate the media items.
-    page.locator('#media-items')
-
     await expect(page.locator('.media-list-title')).toHaveText('img')
   })
 })
