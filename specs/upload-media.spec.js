03bfb87fda75a350c56b0c1cb7b9ef423cf453d0
pavanpatil1 <=>
2022-08-01 12:28:25 +0530

    Fix formatting

--- specs/upload-media.spec.js
+++ specs/upload-media.spec.js
@@ -1,23 +1,23 @@
 /**
  * WordPress dependencies
  */
-const { test, expect } = require('@wordpress/e2e-test-utils-playwright')
+const { test, expect } = require( '@wordpress/e2e-test-utils-playwright' )
 
-test.describe('Upload Media', () => {
-  test('Should able to upload media', async ( { page, admin } ) => {
-    await admin.visitAdminPage('media-new.php')
+test.describe( 'Upload Media', () => {
+  test( 'Should able to upload media', async ( { page, admin } ) => {
+    await admin.visitAdminPage( 'media-new.php' )
 
     await expect(page.locator( "div[class='wrap'] h1" )).toHaveText( 'Upload Media' ) //Wait for Upload media text
 
     const [fileChooser] = await Promise.all([
       // It is important to call waitForEvent before click to set up waiting.
-      page.waitForEvent('filechooser'),
+      page.waitForEvent( 'filechooser' ),
       // Opens the file chooser.
-      page.locator('role=button[name="Select Files"i]').click()
+      page.locator( 'role=button[name="Select Files"i]' ).click()
     ])
-    await fileChooser.setFiles(['uploads/img.jpg'])
+    await fileChooser.setFiles([ 'uploads/img.jpg' ])
 
     //Validate the media items.
-    await expect(page.locator('.media-list-title')).toHaveText('img')
+    await expect(page.locator( '.media-list-title' )).toHaveText( 'img' )
   })
 })
